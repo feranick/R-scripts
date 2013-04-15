a76cc4ff588237da3babeaa1729f2247bb7f0498
Feranick <feranick@hotmail.com>
2013-04-15 16:02:10 -0400

    First Release: matrix_to_column and format_for_cluster scripts.

---
+++ README.md
@@ -0,0 +1,1 @@
+R-scripts, mainly for clustering analysis.
---
+++ matrix_to_column/matrix-to-column.R
@@ -0,0 +1,67 @@
+##########################################################
+#
+# Conversion of LabSpec Maps/Matrices into single columns
+#
+# Version 2-20130415a
+#
+# Nicola Ferralis - ferralis@mit.edu
+#
+# Released under GNU Public License v.3
+#
+##########################################################
+
+inputFile="simplem.txt"
+XName="X"
+YName="Y"
+csvOut=T  # Set to false is normal txt output
+
+# Headers and Definitions
+parValue=gsub(".txt","",inputFile)
+if(csvOut==TRUE){
+    outputFile=paste(parValue,"-col.csv",sep="")} else {
+        
+        outputFile=paste(parValue,"-col.txt",sep="")}
+
+# Get and Set current working directory
+(WD <- getwd())
+if (!is.null(WD)) setwd(WD)
+print(WD)
+
+# Read Y coordinates from File
+m1=read.table(inputFile, header = FALSE, nrow =1)
+x1 <- matrix(scan(inputFile, n = nrow(m1)*ncol(m1)), nrow(m1), ncol(m1), byrow = TRUE)
+
+# Reorder coordinates to match the matrix
+for(i in seq(ncol(x1),1,-1)){
+        x1[i+1]=x1[i]
+        }
+x1[1]=0
+
+# Read data in matrix
+m = read.table(inputFile, header = FALSE, skip =1)
+x <- matrix(scan(inputFile, n = nrow(m)*ncol(m), skip=1), nrow(m), ncol(m), byrow = TRUE)
+
+# Define new matrix
+a=matrix(NA , (nrow(x))*(ncol(x)-1), 4)
+
+# Define header for new matrix
+colnames(a) <- c("",parValue,XName,YName)
+print(colnames(a))
+
+# Transfer data into new matrix
+k=1
+for(i in 1:nrow(x)){
+    for(j in 2:ncol(x)){
+        
+        a[k,1]=k
+        a[k,2]=x[i,j]
+        a[k,3]=x[i,1]
+        a[k,4]=x1[j]
+        k=k+1
+    }}
+
+# Save new file
+#write.table(a, file = outputFile, col.names = T, row.names = F)
+if(csvOut==TRUE){
+    write.csv(a, file = outputFile, row.names = F)} else {
+        write.table(a, file = outputFile, quote = FALSE, sep = "\t", col.names = T, row.names = F)}
